506ce548dd6fb36e988fc50b8510ff91bf9ec20b
vdeo <vdeo@naoj.org>
2021-03-03 16:34:36 -1000

    Lots of placeholder files

---
+++ scripts/scxkw-initdb.py
@@ -0,0 +1,9 @@
+import 
+
+# TODO: remove the extension of the file
+
+depends: redis-py, redisworks (github)
+
+F16: dec base 10 strings up to 16 chars (signed, dot, exponents)
+I16: dec base 10 strings up to 16 chars (signed)
+A16: string up to 16
---
+++ setup.py
@@ -0,0 +1,5 @@
+'''
+    I want the repo to work with pip install -e .
+
+    Can just grep the imports to keep track of the dependencies
+'''
