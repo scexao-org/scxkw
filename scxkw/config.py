f1b2435995c303d977aad719859bb71e1e5cb95c
vdeo@scexao <vdeo@naoj.org>
2021-03-05 15:46:51 -1000

    Change config to scexao computers

--- scxkw/config.py
+++ scxkw/config.py
@@ -5,12 +5,13 @@
 '''
 
 # Redis location - needs to be updated in the conf file as well
-REDIS_DB_HOST = '127.0.0.1'
+REDIS_DB_HOST = '133.40.161.194'
 REDIS_DB_PORT = 6379
 
 import os
 FITS_LOCATION = os.environ['MILK_SHM_DIR'] # Straight to the tmpfs
 
-REDIS_CONF_PATH='/home/vdeo/src/scxkw/conf/redis_dbconf.conf'
+REDIS_CONF_PATH='/home/scexao/src/scxkw/conf/redis_dbconf.conf'
+#REDIS_CONF_PATH='/home/vdeo/src/scxkw/conf/redis_dbconf.conf'
 # Actually we need to use a TSV instead of a CSV, because the data contains commas...
-KEYWORD_CSV_PATH='/home/vdeo/src/scxkw/conf/scxkw.tsv'
+KEYWORD_CSV_PATH='/home/scexao/src/scxkw/conf/scxkw.tsv'
