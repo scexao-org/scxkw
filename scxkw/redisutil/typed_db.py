523810d435d9e7d539ac16fe942fc7889b25f49f
vdeo <vdeo@naoj.org>
2021-03-05 15:22:08 -1000

    Config file, typed redis pipeline, setup.py

--- scxkw/redisutil/typed_db.py
+++ scxkw/redisutil/typed_db.py
@@ -3,24 +3,34 @@ import redis
 from .type_cast import nested_cast
 
 
-def func_factory(method_name):
-
+def func_factory(method_name, superclass):
     def method(self, *args, **kwargs):
-        superclass_method = getattr(redis.Redis, method_name)
+        superclass_method = getattr(superclass, method_name)
         ret = superclass_method(self, *args, **kwargs)
         return nested_cast(ret)
 
     return method
 
 
-class Redis(redis.Redis):
+class Pipeline(redis.client.Pipeline):
     pass
 
 
+class Redis(redis.Redis):
+    
+    def pipeline(self, transaction=True, shard_hint=None):
+        return Pipeline(
+            self.connection_pool,
+            self.response_callbacks,
+            transaction,
+            shard_hint)
+
+
 METHODS_TO_CAST = [
-    'get', 'hget', 'hmget', 'hgetall', 'scan', 'hscan', 'sadd', 'smembers', 'sinter',
-    'sunion', 'exists', 'hexists', 'keys'
+    'get', 'hget', 'hmget', 'hgetall', 'scan', 'hscan', 'sadd', 'smembers',
+    'sinter', 'sunion', 'exists', 'hexists', 'keys', 'execute'
 ]
 
 for method_name in METHODS_TO_CAST:
-    setattr(Redis, method_name, func_factory(method_name))
+    setattr(Redis, method_name, func_factory(method_name, redis.Redis))
+    setattr(Pipeline, method_name, func_factory(method_name, redis.client.Pipeline))
--- setup.py
+++ setup.py
@@ -4,4 +4,19 @@
     Can just grep the imports to keep track of the dependencies
 '''
 
-depends redis-py # maybe redisworks ?
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+        name = 'scxkw',
+        version = '0.1',
+        description = 'Keywork management package for SCExAO, between instrument, FITS headers, and Gen2',
+        long_description = long_description,
+        author = 'Vincent Deo',
+        author_email = 'vdeo@naoj.org',
+        url = "http://www.github.com/scexao-org/scxkw",
+        packages = ['scxkw'],  # same as name
+        install_requires = ['docopt', 'numpy', 'redis'],
+        scripts = [])
