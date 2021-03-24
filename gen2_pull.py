0e54a13014f079d86615266f5e4784a08900a936
vdeo@scexao <vdeo@naoj.org>
2021-03-23 18:54:58 -1000

    modified Gen2_pull to include custom Waveplate status

--- gen2_pull.py
+++ gen2_pull.py
@@ -45,20 +45,58 @@ if __name__ == '__main__':
         values = pipe.execute()
         
     dict_to_pull = {k: v for k,v in zip(values[::2], values[1::2])}
+    
+    g2map = rdb.hgetall('map:g2_lookup')
         
     while True:
-        # =========================
+        # ========================
         # NOW PULL FROM GEN2
         # ========================
         
         pulled_from_gen2 = stobj.fetch(dict_to_pull)
+        pulled_for_pipe = {g2map[key]: pulled_from_gen2[key] for key in pulled_from_gen2}
 
         # ========================
         # SETTING VALUES
         # ========================
         with rdb.pipeline() as pipe:
-            for key in pulled_from_gen2:
-                pipe.hset(key, 'value', pulled_from_gen2[key])
+            for key in pulled_for_pipe:
+                pipe.hset(key, 'value', pulled_for_pipe[key])
+            
+            # ========================
+            # WAVEPLATE SPECIFIC KEYS
+            # ========================
+            
+            pipe.hset('POL-ANG1', 'value', 0)
+            
+            stgps1 = float(pulled_for_pipe['P_STGPS1'])
+            if stgps1 == 0:
+                pipe.hset('POLARIZ1', 'value', 'NONE')
+            elif stgps1 == 56:
+                pipe.hset('POLARIZ1', 'value', 'WireGrid(TIR)')
+            elif stgps1 == 90:
+                pipe.hset('POLARIZ1', 'value', 'WireGrid(NIR)')
+            else:
+                pipe.hset('POLARIZ1', 'value', 'UNKNOWN')
+
+            stgps2 = float(pulled_for_pipe['P_STGPS2'])
+            if stgps2 == 0:
+                pipe.hset('RETPLAT1', 'value', 'NONE')
+            elif stgps2 == 56:
+                pipe.hset('RETPLAT1', 'value', 'HWP(NIR)')
+            else:
+                pipe.hset('RETPLAT1', 'value', 'UNKNOWN')
+                
+            stgps3 = float(pulled_for_pipe['P_STGPS3'])
+            if stgps3 == 0:
+                pipe.hset('RETPLAT2', 'value', 'NONE')
+            elif stgps3 == 56:
+                pipe.hset('RETPLAT2', 'value', 'HWP(TIR)')
+            elif stgps3 == 90:
+                pipe.hset('RETPLAT2', 'value', 'QWP(NIR)')
+            else:
+                pipe.hset('RETPLAT2', 'value', 'UNKNOWN')
+            
             pipe.execute()
 
         time.sleep(10.0)
