66715f66973ebc39f9f9395c42b43f1237e7f4de
vdeo@scexao6 <vdeo@naoj.org>
2022-02-11 21:30:50 +0000

    CHARIS server header tester default port

--- scripts/fetch_charis_fits_test.py
+++ scripts/fetch_charis_fits_test.py
@@ -3,7 +3,7 @@
 import pickle
 import logging
 import socket
-import sys
+import os, sys
 
 
 import astropy.io.fits as pyfits
@@ -13,7 +13,12 @@ def main():
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)-15s %(levelno)-2s %(message)s')
 
-    s = socket.create_connection(('localhost', int(sys.argv[1])), 5)
+    if len(sys.argv) == 1:
+        PORT = 18447
+    else:
+        PORT = int(sys.argv[1])
+
+    s = socket.create_connection((os.getenv('IP_SC2'), PORT), 5)
     s.send(b'hdr\n')
 
     allData = b''
