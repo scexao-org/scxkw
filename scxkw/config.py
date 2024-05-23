107c071b73cdd240f6f356fa54a44c1b88395478
Miles Lucas <mdlucas@hawaii.edu>
2024-05-23 21:00:34 +0000

    update config

--- scxkw/config.py
+++ scxkw/config.py
@@ -22,11 +22,10 @@ except:
     FITS_HEADER_PATH = None
 
 # Where to write the data dump every now and then
-if os.environ.get("WHICHCOMP", "0") not in ("5", "6"):
+CSV_DUMP_PATH = '/home/scexao/logdir/' # + date + csv file name
+if not os.environ.get("WHICHCOMP", "0") == "6":
     # scexao2 fallback !
     CSV_DUMP_PATH = '/media/data'
-else:
-    CSV_DUMP_PATH = '/home/scexao/logdir/' # + date + csv file name
 
 REDIS_CONF_PATH='/home/scexao/src/scxkw/conf/redis_dbconf.conf'
 #REDIS_CONF_PATH='/home/vdeo/src/scxkw/conf/redis_dbconf.conf'
@@ -45,14 +44,10 @@ if TEST:
     GEN2PATH_PRELIM = "/tmp/ARCHIVE0"
     GEN2PATH_NODELETE = "/tmp/ARCHIVE1"
     GEN2PATH_OKDELETE = "/tmp/ARCHIVE2"
-elif os.environ['WHICHCOMP'] == '6':
-    GEN2PATH_PRELIM = "/mnt/tier1/PRE_ARCHIVE_DATA/" # <- ARCHIVE in here # OBSOLETE
+else:
+    GEN2PATH_PRELIM = "/mnt/tier1/PRE_ARCHIVE_DATA/" # <- ARCHIVE in here
     GEN2PATH_NODELETE = "/mnt/tier1/ARCHIVED_DATA/" # <- after sync and deinterleave
     GEN2PATH_OKDELETE = "/mnt/tier1/2_ARCHIVED_DATA/" # <- after frameIDs and fpack, etc.
-elif os.environ['WHICHCOMP'] == '5':
-    GEN2PATH_PRELIM = "/mnt/fuuu/PRE_ARCHIVE_DATA/" # <- ARCHIVE in here # OBSOLETE
-    GEN2PATH_NODELETE = "/mnt/fuuu/ARCHIVED_DATA/" # <- after sync and deinterleave
-    GEN2PATH_OKDELETE = "/mnt/fuuu/2_ARCHIVED_DATA/" # <- after frameIDs and fpack, etc.
 
 # streamname: archive letter mapping
 CAMIDS = {
