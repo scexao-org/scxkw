97fd1c0f02215b229f04acd95177f400f0974d62
scexao5 <scexao5@naoj.org>
2025-06-26 07:23:22 +0000

    switch to simpler, dictionary-based logic rather than dataclass

--- scxkw/tools/vampires_archiving.py
+++ scxkw/tools/vampires_archiving.py
@@ -1,4 +1,3 @@
-import dataclasses
 from datetime import datetime, timezone
 import pandas
 import pathlib
@@ -13,31 +12,23 @@ class WrongComputerException(BaseException):
     """Use to safegaurd scripts to run on specific computers"""
 
 
-@dataclasses.dataclass
-class VAMPIRESArchiveTelemetry:
-    scexao5_path: str
-    utc_date: str
 
-	# monitoring the syncdeint processing
-    processed: bool
-    processed_timestamp: str # date processed
+def empty_entry(path: pathlib.Path):
+    # sanitize inputs
+    path_str = str(path.absolute())
+    utc_date = _date_from_path(path).strftime("%Y-%m-%d")
 
-	# monitoring the sc5 -> sc6 transfer
-    transferred_to_scexao6: bool
-    transferred_timestamp: str
-
-    @classmethod
-    def initialize(__cls__, path: str):
-
-        proto = __cls__(
-            scexao5_path = path,
-            utc_date = _date_from_path(pathlib.Path(path)).strftime("%Y-%m-%d"),
-            processed=False,
-            processed_timestamp="",
-            transferred_to_scexao6=False,
-            transferred_timestamp=""
-        )
-        return proto
+    entry = {
+        "scexao5_path" : path_str,
+        "utc_date" :  utc_date,
+        "processed" : False,
+        "processed_timestamp" : None,
+        "transferred_to_scexao6" : False,
+        "transferred_timestamp" : None,
+        "safe_on_scexao6" : False,
+        "safe_timestamp" : None
+    }
+    return entry
 
 
 def _date_from_path(path: pathlib.Path) -> datetime:
@@ -54,28 +45,37 @@ def load_table() -> pandas.DataFrame | None:
         print("WARNING: No database CSV found!")
         return None
 
-def check_for_new_folders(path: pathlib.Path=ARCHIVE_DIR):
-    # step 1, get all candidate folders
-    folders = filter(lambda p: p.is_dir(), path.iterdir())
+def check_for_new_folders(directory: pathlib.Path=ARCHIVE_DIR):
     pattern = re.compile(r"\d{8}")
-    folder_candidates = [str(folder.absolute()) for folder in folders if pattern.match(folder.name)]
-
-    # step 2, cross match with database and determine which are new
     table = load_table()
     if table is not None:
-        existing_paths = set(table["scexao5_path"])
+        existing_paths = list(map(pathlib.Path, table["scexao5_path"]))
     else:
-        existing_paths = set()
+        existing_paths = []
 
-    new_paths = set(folder_candidates) - existing_paths
-    if len(new_paths) == 0:
+    new_telemetry = []
+    for path in directory.iterdir():
+        # filter 1: it's a directroy
+        if not path.is_dir():
+            continue
+        # filter 2: it's 8 digits, like 20200506
+        if not pattern.match(path.name):
+            continue
+
+        # filter 3: it's not in the table already
+        if path.absolute() in existing_paths:
+            continue
+        telemetry = empty_entry(path)
+        new_telemetry.append(telemetry)
+
+    # step 2, cross match with database and determine which are new
+
+    if len(new_telemetry) == 0:
         msg = "No new archive folders detected"
         print(msg)
         return False
 
-    new_telemetry = list(map(VAMPIRESArchiveTelemetry.initialize, sorted(new_paths)))
-
-    dataframe = pandas.DataFrame(map(dataclasses.asdict, new_telemetry))
+    dataframe = pandas.DataFrame(new_telemetry)
 
     if table is None:
         dataframe.to_csv(ARCHIVE_LOG_PATH, mode="w", index=False)
