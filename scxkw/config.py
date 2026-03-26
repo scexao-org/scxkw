6c157f27a7c55295bf645c52ce57a9e5014a0b94
vdeo <vdeo@naoj.org>
2026-03-26 10:20:07 +0100

    feat: add batched redis hget that returns a dict

--- scxkw/config.py
+++ scxkw/config.py
@@ -3,37 +3,38 @@
 
     To be conveniently imported all around the python scripts
 '''
-
+from __future__ import annotations
+import typing as typ
 
 # Redis location - needs to be updated in the conf file as well
 try:
     import scxconf
     REDIS_DB_HOST = scxconf.REDIS_DB_HOST
     REDIS_DB_PORT = scxconf.REDIS_DB_PORT
-except: # Fallback pre-scxconf
+except:  # Fallback pre-scxconf
     REDIS_DB_HOST = '133.40.161.193'
     REDIS_DB_PORT = 6379
 
 import os
 # Where to write the data-less fits headers
 try:
-    FITS_HEADER_PATH = os.environ['MILK_SHM_DIR'] + '/fits/' # Straight to the tmpfs
+    FITS_HEADER_PATH = os.environ[
+        'MILK_SHM_DIR'] + '/fits/'  # Straight to the tmpfs
 except:
     FITS_HEADER_PATH = None
 
 HOME = os.environ.get("HOME", "/home/scexao")
 # Where to write the data dump every now and then
-CSV_DUMP_PATH = f'{HOME}/logdir/' # + date + csv file name
+CSV_DUMP_PATH = f'{HOME}/logdir/'  # + date + csv file name
 if os.environ.get("WHICHCOMP", "") == "2":
     # scexao2 fallback !
     CSV_DUMP_PATH = '/media/data'
 
-REDIS_CONF_PATH=f'{HOME}/src/scxkw/conf/redis_dbconf.conf'
+REDIS_CONF_PATH = f'{HOME}/src/scxkw/conf/redis_dbconf.conf'
 #REDIS_CONF_PATH='/home/vdeo/src/scxkw/conf/redis_dbconf.conf'
 # Actually we need to use a TSV instead of a CSV, because the data contains commas...
-KEYWORD_CSV_PATH=f'{HOME}/src/scxkw/conf/scxkw.tsv'
+KEYWORD_CSV_PATH = f'{HOME}/src/scxkw/conf/scxkw.tsv'
 #KEYWORD_CSV_PATH='/home/vdeo/src/scxkw/conf/scxkw.tsv'
-
 
 # for testing purposes, please use the simulator
 #GEN2HOST = 'g2sim.subaru.nao.ac.jp'
@@ -46,18 +47,18 @@ if TEST:
     GEN2PATH_NODELETE = "/tmp/ARCHIVE1"
     GEN2PATH_OKDELETE = "/tmp/ARCHIVE2"
 if os.environ.get("WHICHCOMP", "") == "5":
-    GEN2PATH_PRELIM = "/mnt/fuuu/PRE_ARCHIVE_DATA/" # <- ARCHIVE in here
-    GEN2PATH_NODELETE = "/mnt/fuuu/ARCHIVED_DATA/" # <- after sync and deinterleave
-    GEN2PATH_OKDELETE = "/mnt/fuuu/2_ARCHIVED_DATA/" # <- after frameIDs and fpack, etc.
+    GEN2PATH_PRELIM = "/mnt/fuuu/PRE_ARCHIVE_DATA/"  # <- ARCHIVE in here
+    GEN2PATH_NODELETE = "/mnt/fuuu/ARCHIVED_DATA/"  # <- after sync and deinterleave
+    GEN2PATH_OKDELETE = "/mnt/fuuu/2_ARCHIVED_DATA/"  # <- after frameIDs and fpack, etc.
 elif os.environ.get("WHICHCOMP", "") == "6":
-    GEN2PATH_PRELIM = "/mnt/tier1/PRE_ARCHIVE_DATA/" # <- ARCHIVE in here
-    GEN2PATH_NODELETE = "/mnt/tier1/ARCHIVED_DATA/" # <- after sync and deinterleave
-    GEN2PATH_OKDELETE = "/mnt/tier1/2_ARCHIVED_DATA/" # <- after frameIDs and fpack, etc.
+    GEN2PATH_PRELIM = "/mnt/tier1/PRE_ARCHIVE_DATA/"  # <- ARCHIVE in here
+    GEN2PATH_NODELETE = "/mnt/tier1/ARCHIVED_DATA/"  # <- after sync and deinterleave
+    GEN2PATH_OKDELETE = "/mnt/tier1/2_ARCHIVED_DATA/"  # <- after frameIDs and fpack, etc.
 
 # streamname: archive letter mapping
 CAMIDS = {
-    "agen2": "SCXB", # Apapane
-    "pgen2": "SCXC", # Palila
+    "agen2": "SCXB",  # Apapane
+    "pgen2": "SCXC",  # Palila
     #"first": "F", # FIRST (andor and hamamatsu, careful)
     #"glint": "G", # GLINT
     #"ocam2d": "P", # Reno
@@ -75,18 +76,21 @@ class MAGIC_BOOL_STR:
 
     TUPLE = ('#FALSE#', '#TRUE#')
 
+
 class MAGIC_HW_STR:
     HEIGHT = '#HEIGHT#'
     WIDTH = '#WIDTH#'
 
-def redis_check_enabled():
+
+if typ.TYPE_CHECKING:
+    from scxkw.redisutil.typed_db import Redis
+
+
+def redis_check_enabled(
+) -> tuple[Redis, typ.Literal[True]] | tuple[None, typ.Literal[False]]:
     try:
         from scxkw.config import REDIS_DB_HOST, REDIS_DB_PORT, GEN2HOST
         from scxkw.redisutil.typed_db import Redis
-        RDB = Redis(host=REDIS_DB_HOST, port=REDIS_DB_PORT)
-        HAS_REDIS = True
+        return Redis(host=REDIS_DB_HOST, port=REDIS_DB_PORT), True
     except:
-        RDB = None
-        HAS_REDIS = False
-
-    return RDB, HAS_REDIS
+        return None, False
--- scxkw/redisutil/typed_db.py
+++ scxkw/redisutil/typed_db.py
@@ -5,7 +5,6 @@ import redis
 
 import typing as typ
 
-
 from .type_cast import nested_cast, to_redis_scalar_cast, ScxkwValueType
 from ..config import MAGIC_BOOL_STR
 
@@ -21,52 +20,68 @@ def func_factory(method_name, superclass):
         (which could cause infinite recursion)
     '''
     method_to_patch = getattr(superclass, method_name)
+
     def method(self, *args, **kwargs):
         try:
             #1/0
             ret = method_to_patch(self, *args, **kwargs)
         except (redis.exceptions.ConnectionError, ZeroDivisionError):
             print("Running in Redis-less mode - not available")
-            return None # Bad idea?
+            return None  # Bad idea?
         return nested_cast(ret)
 
     return method
 
+
 class Pipeline(redis.client.Pipeline):
-    
-    def __init__(self, connection_pool, response_callbacks, transaction, shard_hint, *, auto_execute: typ.Optional[int] = 50) -> None:
-        super().__init__(connection_pool, response_callbacks, transaction, shard_hint)
+
+    def __init__(self,
+                 connection_pool,
+                 response_callbacks,
+                 transaction,
+                 shard_hint,
+                 *,
+                 auto_execute: typ.Optional[int] = 50) -> None:
+        super().__init__(connection_pool, response_callbacks, transaction,
+                         shard_hint)
 
         self.auto_execute = auto_execute
         self.return_cache = []
 
     def execute_command(self, *args, **options):
-        ret =  super().execute_command(*args, **options)
+        ret = super().execute_command(*args, **options)
         if self.auto_execute and len(self.command_stack) >= self.auto_execute:
             self.return_cache += super().execute()
         return ret
-    
+
     def execute(self, raise_on_error: bool = True) -> list[Any]:
         ret = self.return_cache + super().execute(raise_on_error)
         self.return_cache = []
         return ret
 
-    def hset(self, name: str,
+    def hset(self,
+             name: str,
              key: typ.Optional[str] = None,
              value: typ.Optional[ScxkwValueType] = None,
-             mapping: typ.Optional[typ.Mapping[str, ScxkwValueType]] = None
-             ):
+             mapping: typ.Optional[typ.Mapping[str, ScxkwValueType]] = None):
         '''
         Hack hset for the magic boolean
         '''
         new_value = None if value is None else to_redis_scalar_cast(value)
-        new_mapping = None if mapping is None else {k: to_redis_scalar_cast(mapping[k]) for k in mapping}
-        
-        return redis.client.Pipeline.hset(self, name=name, key=key, value=new_value, mapping=new_mapping)
+        new_mapping = None if mapping is None else {
+            k: to_redis_scalar_cast(mapping[k])
+            for k in mapping
+        }
+
+        return redis.client.Pipeline.hset(self,
+                                          name=name,
+                                          key=key,
+                                          value=new_value,
+                                          mapping=new_mapping)
 
 
 class Redis(redis.Redis):
-    
+
     def __init__(self, *args, **kwargs):
         if not "socket_connect_timeout" in kwargs:
             kwargs["socket_connect_timeout"] = 1.0
@@ -74,31 +89,61 @@ class Redis(redis.Redis):
             kwargs["socket_timeout"] = 1.0
         return redis.Redis.__init__(self, *args, **kwargs)
 
-    def pipeline(self, transaction=False, shard_hint=None, auto_execute: typ.Optional[int] = 50):
-        return Pipeline(
-            self.connection_pool,
-            self.response_callbacks,
-            transaction,
-            shard_hint,
-            auto_execute = auto_execute)
-    
-    def hset(self, name: str,
-            key: typ.Optional[str] = None,
-            value: typ.Optional[ScxkwValueType] = None,
-            mapping: typ.Optional[typ.Mapping[str, ScxkwValueType]] = None
-            ):
+    def pipeline(self,
+                 transaction=False,
+                 shard_hint=None,
+                 auto_execute: typ.Optional[int] = 50):
+        return Pipeline(self.connection_pool,
+                        self.response_callbacks,
+                        transaction,
+                        shard_hint,
+                        auto_execute=auto_execute)
+
+    def hset(self,
+             name: str,
+             key: typ.Optional[str] = None,
+             value: typ.Optional[ScxkwValueType] = None,
+             mapping: typ.Optional[typ.Mapping[str, ScxkwValueType]] = None):
         '''
         Hack hset for the magic boolean
         '''
         new_value = None if value is None else to_redis_scalar_cast(value)
-        new_mapping = None if mapping is None else {k: to_redis_scalar_cast(mapping[k]) for k in mapping}
-        
-        return redis.Redis.hset(self, name=name, key=key, value=new_value, mapping=new_mapping)
+        new_mapping = None if mapping is None else {
+            k: to_redis_scalar_cast(mapping[k])
+            for k in mapping
+        }
+
+        return redis.Redis.hset(self,
+                                name=name,
+                                key=key,
+                                value=new_value,
+                                mapping=new_mapping)
+
+    def redis_batched_hget(
+            self, keys: typ.Iterable[str]) -> typ.Mapping[str, ScxkwValueType]:
+        with self.pipeline() as pipe:
+            for key in keys:
+                redis.hget(key, 'value')
+            all_values = pipe.execute()
+        return {k: v for (k, v) in zip(keys, all_values)}
 
 
 METHODS_TO_CAST = [
-    'get', 'hget', 'hmget', 'hgetall', 'scan', 'hscan', 'sadd', 'smembers',
-    'sinter', 'sunion', 'exists', 'hexists', 'keys', 'execute', 'type',
+    'get',
+    'hget',
+    'hmget',
+    'hgetall',
+    'scan',
+    'hscan',
+    'sadd',
+    'smembers',
+    'sinter',
+    'sunion',
+    'exists',
+    'hexists',
+    'keys',
+    'execute',
+    'type',
 ]
 
 for method_name in METHODS_TO_CAST:
@@ -108,5 +153,3 @@ for method_name in METHODS_TO_CAST:
     # So take Pipeline.execute, wrap and overset Pipeline.execute.
     if hasattr(Pipeline, method_name):
         setattr(Pipeline, method_name, func_factory(method_name, Pipeline))
-
-
